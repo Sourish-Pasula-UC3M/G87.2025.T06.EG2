ae9549e9986842e8a2186a3b09ca5e23dcc5cc38
nikhil <np0610@berkeley.edu>
2025-03-25 15:52:38 +0100

    edit

--- src/main/python/uc3m_money/account_manager.py
+++ src/main/python/uc3m_money/account_manager.py
@@ -7,8 +7,18 @@ class AccountManager:
 
     @staticmethod
     def validate_iban(iban: str):
-        """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
-        OR FALSE IN OTHER CASE"""
+        """
+            Validate the format of a Spanish IBAN.
+
+            An IBAN is considered valid if it starts with 'ES', has a total length of 24 characters,
+            and the characters after 'ES' are all digits.
+
+            Args:
+                iban (str): The IBAN string to validate.
+
+            Returns:
+                bool: True if the IBAN is valid, False otherwise.
+            """
         if iban.startswith("ES") and len(iban) == 24 and iban[2:].isdigit():
             return True
         return False
